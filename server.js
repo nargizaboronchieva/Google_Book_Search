2308ff2587e06d2b55b2fdcc1fff068fd9beeb30
Nargiza Boronchieva <nargizaboronchieva@gmail.com>
2020-10-08 21:31:46 -0400

    test

--- server.js
+++ server.js
@@ -5,21 +5,29 @@ const routes= require ("./routes")
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Define middleware here
+// Defining middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve up static assets (usually on heroku)
+// Serve up static assets- Heroku
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+// Defining API routes here
 app.use(routes)
 
-//Connet to mongoose
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {useNewUrlParser: true});
-
+//Connecting  to mongoose
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {useNewUrlParser: true});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
