eb6b51b79ab2728c678383d839606e986b6cac77
Nargiza Boronchieva <nargizaboronchieva@gmail.com>
2020-10-08 20:18:49 -0400

    added build pack

---
+++ client/build/precache-manifest.cc05a07e350a2e09ede6168c47087c7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea3eaee471510fb1add4",
+    "url": "/static/css/main.1b0c347c.chunk.css"
+  },
+  {
+    "revision": "ea3eaee471510fb1add4",
+    "url": "/static/js/main.36614f1f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d9eb6bb64f27f3227e15",
+    "url": "/static/js/2.fc369e8d.chunk.js"
+  },
+  {
+    "revision": "f464ef125a2f2f325767802b18b6bfe6",
+    "url": "/static/media/books-header.f464ef12.jpg"
+  },
+  {
+    "revision": "a7efcae9f2856173aeca9ef99b2a1760",
+    "url": "/index.html"
+  }
+];
